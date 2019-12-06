b469c20772918268a5cd12bf5575347d677d7d75
Mark <markke4@outlook.com>
2019-12-06 16:45:04 +1100

    finished working with dates

---
+++ 07_workingWithDates/59_dateTime/59_dateTime/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _59_dateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dateTime is defined in the system namespace
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now; //a static property of dateTime structure.
+            var today = DateTime.Today; //if you just want todays date, irrespective of the time
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            //date time objects in c# are immutable, aka once you create them... you cannot change them. So how do you modify them?
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            //a datetime object has a bunch of methods for converting it to a date or time string.
+
+            Console.WriteLine(now.ToLongDateString()); //returns "Friday, 6th of december 2019"
+            Console.WriteLine(now.ToShortDateString()); //returns "6/12/19"
+
+            Console.WriteLine();
+
+            Console.WriteLine(now.ToLongTimeString()); //returns "4:18:25 pm"
+            Console.WriteLine(now.ToShortTimeString()); //returns "4:18 pm"
+
+
+            //to display both date AND time:
+            Console.WriteLine(now.ToString());
+            //with format specifier
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH-mm"));
+        }
+    }
+}
---
+++ 07_workingWithDates/60_timeSpan/60_timeSpan/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _60_timeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //CREATING TIMESPAN OBJECTS:
+
+            //timespan represents a length of time
+            var timeSpan = new TimeSpan(1, 2, 3);
+            //this overload takes in 3 arguments, hours minutes seconds
+
+            var timeSpan2 = new TimeSpan(1, 0, 0);
+
+            Console.WriteLine(timeSpan);
+            Console.WriteLine(timeSpan2);
+
+            //another way to create a timespan: 
+            var timeSpan3 = TimeSpan.FromHours(1);
+
+            Console.WriteLine(timeSpan3);
+
+            //3rd way to create a timespan:
+            //if you subtract two datetime objects, the result is a timespan.
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+
+            var duration = end - start;
+            Console.WriteLine(duration);
+
+            //READING TIMESPAN PROPERTIES:
+            Console.WriteLine("Minutes: " + timeSpan.Minutes); //returns minutes component of timespan object (i.e. 2 in this example, line 13)
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes); //converts total timespan into minutes, i.e. 60 + 2 + 0.3
+
+            //ADD TIMESPAN
+            //similar to datetime, the timespan is immutable .. once you create it, you cannot change it.
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8))); //added 8 minutes to original timespan
+
+            //SUBTRACT TIMESPAN
+            Console.WriteLine("Subtract: " + timeSpan.Add(TimeSpan.FromMinutes(2)));
+
+            //Conversion too and from strings:
+            //simply call the ToString() method
+            Console.WriteLine("To String:" + timeSpan.ToString());
+
+            //to convert from string to timespan:
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+        }
+    }
+}
